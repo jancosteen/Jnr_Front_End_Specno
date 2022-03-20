8e9171311e33a938677f59dc8ee10774755bac9c
Janco Steenkamp <jancosteenkamp3@gmail.com>
2022-03-20 12:37:42 +0200

    Card_and_Flip_Animation

--- angular-memory/src/app/app.module.ts
+++ angular-memory/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CardComponent } from './app/components/card/card.component';
+import { CardComponent } from './components/card/card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent
+
   ],
   imports: [
     BrowserModule,
--- angular-memory/src/app/components/card/card.component.ts
+++ angular-memory/src/app/components/card/card.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CardData } from 'src/app/interfaces/cardData.interface';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('cardFlip', [
+      state('default', style({
+        transform: 'none'
+      })),
+      state('flipped', style({
+        transform: 'rotateY(180deg)'
+      })),
+      transition('default => flipped', [
+        animate('400ms')
+      ]),
+      transition('flipped => default', [
+        animate('200ms')
+      ])
+    ])
+  ]
 })
 export class CardComponent implements OnInit {
+
+  data: CardData = {
+    imageId: "red_joker",
+    state: "default"
+  };
+
+  //@Output() cardClicked = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cardClicked() {
+    if (this.data.state === "default") {
+      this.data.state = "flipped";
+    } else {
+      this.data.state = "default";
+    }
   }
 
 }
